47f8e7cf4a45917aaf10d85d685e7acbcf8a836a
Hoang-Minh Nguyen <alucard522@hotmail.com>
2014-09-26 00:14:30 -0500

    Updates app folders, included msx and routes

---
+++ HelloHybrid/app/routes/app.route.js
@@ -0,0 +1,23 @@
+﻿m.route(document.body, "/", {
+    "/": home,
+    "/login": login,
+    "/dashboard": dashboard,
+});
+
+//a sample module
+var dashboard = {
+    controller: function () {
+        this.id = m.route.param("userID");
+    },
+    view: function (controller) {
+        return m("div", controller.id);
+    }
+}
+
+//setup routes to start w/ the `#` symbol
+m.route.mode = "hash";
+
+//define a route
+m.route(document.body, "/dashboard/johndoe", {
+    "/dashboard/:userID": dashboard
+});
